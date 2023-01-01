a94476de0501b74dc56537df438aa11acfb9e20d
AhmadAlaaeldin <ahmad.alaaeldinn@gmail.com>
2023-01-01 12:27:19 -0800

    connection to another url

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,6 @@
 const mongoose  = require("mongoose")
 mongoose.set('strictQuery', true);
-mongoose.connect("mongodb://localhost:27017")
+mongoose.connect("mongodb://localhost:27017/FlightBooking")
 .then(()=>{
     console.log("mongo db connected");
 })
